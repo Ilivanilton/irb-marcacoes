078733fd3b9688d98e5ad5bac4119ef19cea00ef
ilivanilton <ilivanilton@gmail.com>
2025-04-27 18:07:06 -0300

    initial commit

---
+++ src/main/java/com/irb/marcacoes/MarcacoesApplication.java
@@ -0,0 +1,13 @@
+package com.irb.marcacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcacoesApplication.class, args);
+	}
+
+}
